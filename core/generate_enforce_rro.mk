crdroidandroid/android_build

build: overriding lineage namespace
Change-Id: Ic1cd50db6a194193b844773d8972955010cd4894
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/generate_enforce_rro.mk
@@ -1,6 +1,8 @@
 include $(CLEAR_VARS)
 
-enforce_rro_module := $(enforce_rro_source_module)__$(PRODUCT_NAME)__auto_generated_rro_$(enforce_rro_partition)
+enforce_rro_product_name := $(subst lineage_,aosp_,$(PRODUCT_NAME))
+enforce_rro_override_module := $(subst Lineage,AOSP,$(subst lineage,aosp,$(subst lineageos,aosp,$(enforce_rro_source_module))))
+enforce_rro_module := $(enforce_rro_override_module)__$(enforce_rro_product_name)__auto_generated_rro_$(enforce_rro_partition)
 LOCAL_PACKAGE_NAME := $(enforce_rro_module)
 
 intermediates := $(call intermediates-dir-for,APPS,$(LOCAL_PACKAGE_NAME),,COMMON)

Project-Mist-OS/build

Signed-off-by: Dmitrii <bankersenator@gmail.com>

core/generate_enforce_rro.mk
@@ -1,6 +1,8 @@
 include $(CLEAR_VARS)
 
-enforce_rro_module := $(enforce_rro_source_module)__$(PRODUCT_NAME)__auto_generated_rro_$(enforce_rro_partition)
+enforce_rro_product_name := $(subst lineage_,mist_,$(PRODUCT_NAME))
+enforce_rro_override_module := $(subst Lineage,Mist,$(subst lineage,mist,$(subst lineageos,mist,$(enforce_rro_source_module))))
+enforce_rro_module := $(enforce_rro_override_module)__$(enforce_rro_product_name)__auto_generated_rro_$(enforce_rro_partition)
 LOCAL_PACKAGE_NAME := $(enforce_rro_module)
 
 intermediates := $(call intermediates-dir-for,APPS,$(LOCAL_PACKAGE_NAME),,COMMON)